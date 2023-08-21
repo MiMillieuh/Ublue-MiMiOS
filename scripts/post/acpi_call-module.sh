0866d1f39b1716f7596850a7d872dc3da0718911
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-08-21 13:53:00 +0200

    Update acpi_call-module.sh

--- scripts/post/acpi_call-module.sh
+++ scripts/post/acpi_call-module.sh
@@ -6,4 +6,6 @@ RELEASE="$(rpm -E '%fedora')"
 
 mkdir -p /usr/lib/modules/${KERNEL}/extra/acpi_call/
 
-cp acpi_call.ko /usr/lib/modules/6.4.11-200.fc38.x86_64/extra/acpi_call/
+wget https://github.com/MiMillieuh/Ublue-MiMiOS/raw/live/modules/acpi_call.ko
+
+mv acpi_call.ko /usr/lib/modules/6.4.11-200.fc38.x86_64/extra/acpi_call/
