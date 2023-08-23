39d9b10e862e1222f67ac6a2ea902e81a6a1328f
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-08-23 14:01:50 +0200

    Update tuxedo-contols.sh

--- scripts/post/tuxedo-contols.sh
+++ scripts/post/tuxedo-contols.sh
@@ -11,7 +11,7 @@ cd tuxedo-keyboard
 git checkout release
 
 sed -i 's|MAKE\[0\]="make KDIR=/lib/modules/${kernelver}/build"|MAKE[0]="make KDIR=/lib/modules/${KERNEL}/build"|' dkms.conf
-sed -i 's|dkms install -m \$(MODULE_NAME) -v \$(VER)|dkms install -m --kernelsourcedir=/lib/modules/${KERNEL}/source \$(MODULE_NAME) -v \$(VER)|' Makefile
+sed -i 's|dkms install -m \$(MODULE_NAME) -v \$(VER)|dkms install -m --kernelsourcedir=/usr/src/kernels/${KERNEL} \$(MODULE_NAME) -v \$(VER)|' Makefile
 
 make clean && make
 
