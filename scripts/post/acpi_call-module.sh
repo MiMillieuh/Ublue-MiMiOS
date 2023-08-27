23a6298b6b13a4be229397249f9283f8d90fb90f
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-08-27 22:52:20 +0200

    Update acpi_call-module.sh

--- scripts/post/acpi_call-module.sh
+++ scripts/post/acpi_call-module.sh
@@ -8,6 +8,6 @@ mkdir -p /usr/lib/modules/${KERNEL}/extra/acpi_call/
 
 wget https://github.com/MiMillieuh/Ublue-MiMiOS/raw/live/modules/acpi_call.ko
 
-mv acpi_call.ko /usr/lib/modules/6.4.11-200.fc38.x86_64/extra/acpi_call/
+mv acpi_call.ko /usr/lib/modules/${KERNEL}/extra/acpi_call/
 
 depmod ${KERNEL}
