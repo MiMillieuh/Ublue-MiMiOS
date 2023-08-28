447880fb5bc48dc5281482cfb11660227dc1393d
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-08-28 11:06:10 +0200

    Update acpi_call-module.sh

--- scripts/post/acpi_call-module.sh
+++ scripts/post/acpi_call-module.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-ARCH="$(rpm -E '%_arch')"
-KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-RELEASE="$(rpm -E '%fedora')"
+# Récupération des variables cibles
+ARCH=$(rpm -E '%_arch')
+KERNEL=$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
+RELEASE=$(rpm -E '%fedora')
 
+# Décompressez le fichier acpi_call-master.zip
+wget https://github.com/nix-community/acpi_call/archive/refs/heads/master.zip
+
+unzip acpi_call-master.zip -d /tmp
+
+# Aller dans le répertoire décompressé
+cd acpi_call-master
+
+# Compiler le module pour le noyau cible
+make -C /usr/src/kernels/${KERNEL} M=$(pwd) modules
+
+# Vérifier si la compilation a réussi
+if [ $? -ne 0 ]; then
+    echo "Erreur lors de la compilation du module acpi_call."
+    exit 1
+fi
+
+# Déplacer le module au bon emplacement
 mkdir -p /usr/lib/modules/${KERNEL}/extra/acpi_call/
-
-wget https://github.com/MiMillieuh/Ublue-MiMiOS/raw/live/modules/acpi_call.ko
-
 mv acpi_call.ko /usr/lib/modules/${KERNEL}/extra/acpi_call/
 
-depmod ${KERNEL}
+# Afficher un message de confirmation
+echo "Le module acpi_call a été compilé et déplacé avec succès pour le noyau ${KERNEL}."
+
